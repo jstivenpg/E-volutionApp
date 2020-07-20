1fe04fdbcac6e2fbe572edca82ebe15eb1de9bfc
JStivenpgSGM <jopachon@personalsoft.com.co>
2020-07-20 01:39:31 -0500

    Se agregaron campos de prioridad y fecha de vencimiento

--- src/models/Task.js
+++ src/models/Task.js
@@ -5,6 +5,8 @@ const TaskSchema = new Schema({
     title: { type: String, required: true},
     description: { type: String, required: true},
     date: { type: Date, default: Date.now},
+    priority: { type: String, require: true },
+    fecha_vencimiento: { type: String, require: true},
     user: { type: String}
 });
 
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -10,22 +10,30 @@ router.get('/tasks/add', isAuthenticated,  (req, res) => {
 });
 
 router.post('/tasks/add', isAuthenticated, async(req, res) => {
-    const { title, description } = req.body;
+    const { title, description, priority, fecha_vencimiento } = req.body;
     const errors = [];
     if(!title){
-        errors.push({text: 'Please Write a Title'});
+        errors.push({text: 'Por favor escribe un titulo'});
     }
     if(!description){
-        errors.push({text: ' Please Write a Description'})
+        errors.push({text: ' Por favor escribe una descripción'})
+    }
+    if(!priority){
+        errors.push({text: 'Seleccionar una prioridad'});
+    }
+    if(!fecha_vencimiento){
+        errors.push({text: ' Seleccionar fecha de vencimiento'})
     }
     if(errors.length > 0){
         res.render('tasks/add', {
             errors,
             title,
-            description
+            description,
+            priority,
+            fecha_vencimiento
         });
     } else {
-        const newTask = new Task({ title, description });
+        const newTask = new Task({ title, description, priority, fecha_vencimiento });
         newTask.user = req.user.id;
         await newTask.save();
         req.flash('success_msg', 'Tarea agregada con éxito')
@@ -45,7 +53,7 @@ router.get('/tasks/edit/:id', isAuthenticated, async(req, res) => {
 
 router.put('/tasks/edit/:id', isAuthenticated, async(req, res) => {
     const { title, description } = req.body;
-    await Task.findByIdAndUpdate(req.params.id, { title, description}).lean();
+    await Task.findByIdAndUpdate(req.params.id, { title, description, priority, fecha_vencimiento}).lean();
     req.flash('success_msg', 'Tarea actualizada éxitosamente.')
     res.redirect('/tasks');
 });
